2960a5b2aa55f007bef00ddf1e36e45bbe8276bc
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-10-18 17:28:13 -0700

    Cart keeps in localStorage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,14 +2,25 @@ import { configureStore } from '@reduxjs/toolkit';
 import filterSlice from './slices/filterSlice';
 import cartSlice from './slices/cartSlice';
 import pizzaSlice from './slices/pizzaSlice';
+import { loadState, saveState } from '../utils/localStorage';
+
+const preloadedState = loadState();
 
 const store = configureStore({
   reducer: {
+		// @ts-ignore
 		filterSlice,
 		cartSlice,
 		pizzaSlice,
 	},
+	preloadedState,
 })
+
+store.subscribe(() => {
+  saveState({
+    cartSlice: store.getState().cartSlice,
+  });
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
